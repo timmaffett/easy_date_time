0cb3d026fcef419a043b70439afb314faffdf920
MasterHiei <ruhrg.lcx@outlook.com>
2025-12-20 19:41:13 +0800

    refactor!: remove deprecated global functions
    - remove initializeTimeZone/isTimeZoneInitialized
    
    - remove setDefaultLocation/getDefaultLocation/clearDefaultLocation
    
    - remove deprecated functions tests

--- lib/src/easy_date_time_init.dart
+++ lib/src/easy_date_time_init.dart
@@ -1,46 +1,6 @@
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart';
 
-/// Initializes the IANA timezone database.
-///
-/// **Deprecated**: Use [EasyDateTime.initializeTimeZone] instead.
-///
-/// **Must be called before using [EasyDateTime].**
-///
-/// Call once at app startup:
-/// ```dart
-/// void main() {
-///   EasyDateTime.initializeTimeZone();
-///   runApp(MyApp());
-/// }
-/// ```
-@Deprecated(
-  'Use EasyDateTime.initializeTimeZone() instead. '
-  'This function will be removed in v0.4.0.',
-)
-void initializeTimeZone() {
-  tz.initializeTimeZones();
-}
-
-/// Checks if the IANA timezone database has been initialized.
-///
-/// **Deprecated**: Use [EasyDateTime.isTimeZoneInitialized] instead.
-///
-/// Returns `true` if [initializeTimeZone] has been called successfully.
-@Deprecated(
-  'Use EasyDateTime.isTimeZoneInitialized instead. '
-  'This getter will be removed in v0.4.0.',
-)
-bool get isTimeZoneInitialized {
-  try {
-    getLocation('UTC');
-
-    return true;
-  } catch (_) {
-    return false;
-  }
-}
-
 // ============================================================
 // Internal API for EasyDateTime static methods
 // ============================================================
